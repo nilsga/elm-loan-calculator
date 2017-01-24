98718e944058e5ef6696b45773e9be676ece5827
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2017-01-24 23:25:01 +0100

    Starting with material design lite

--- src/Main.elm
+++ src/Main.elm
@@ -1,40 +1,108 @@
+module Main exposing (..)
+
 import Html exposing (..)
-import Html.Events exposing(onInput)
-import Html.Attributes exposing(placeholder)
+import Html.Events exposing (onInput)
+import Html.Attributes exposing (..)
 import LoanCalculations exposing (..)
+import Material.Textfield as Textfield
+import Material.Options as Options
+import Material
 import String
 
-main: Program Never Model Msg
+
+main : Program Never Model Msg
 main =
-  beginnerProgram {
-    model = model,
-    view = view,
-    update = update
-  }
+    program
+        { init = ( model, Cmd.none )
+        , view = view
+        , subscriptions = always Sub.none
+        , update = update
+        }
 
-type alias Model = LoanDetails
-type Msg = LoanAmount String | LoanInterestRate String | LoanDuration String
 
-model: Model
-model = {amount = 2000000, interestRate = 0.025, terms = 12, duration = 20, termFee = 0, initialFee = 0}
+type alias Model =
+    { loanDetails : LoanDetails
+    , mdl : Material.Model
+    }
 
-update: Msg -> Model -> Model
+
+type alias Mdl =
+    Material.Model
+
+
+type Msg
+    = LoanAmount String
+    | LoanInterestRate String
+    | LoanDuration String
+    | Mdl (Material.Msg Msg)
+
+
+model : Model
+model =
+    { loanDetails =
+        { amount = 2000000
+        , interestRate = 0.025
+        , terms = 12
+        , duration = 20
+        , termFee = 0
+        , initialFee = 0
+        }
+    , mdl = Material.model
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg oldModel =
-  case msg of
-    LoanAmount newAmount ->
-      { oldModel | amount = Result.withDefault 2000000 (String.toInt newAmount) }
-    LoanInterestRate newInterestRate ->
-      { oldModel | interestRate = Result.withDefault 0.025 (String.toFloat newInterestRate) }
-    LoanDuration newDuration ->
-      { oldModel | duration = Result.withDefault 20 (String.toInt newDuration) }
+    let
+        oldLoanDetails =
+            oldModel.loanDetails
+    in
+        case msg of
+            LoanAmount newAmount ->
+                ( { oldModel | loanDetails = { oldLoanDetails | amount = Result.withDefault 2000000 <| String.toInt newAmount } }, Cmd.none )
+
+            LoanInterestRate newInterestRate ->
+                ( { oldModel | loanDetails = { oldLoanDetails | interestRate = Result.withDefault 0.025 <| String.toFloat newInterestRate } }, Cmd.none )
+
+            LoanDuration newDuration ->
+                ( { oldModel | loanDetails = { oldLoanDetails | duration = Result.withDefault 20 <| String.toInt newDuration } }, Cmd.none )
+
+            Mdl msg_ ->
+                Material.update Mdl msg_ oldModel
 
 
-view: Model -> Html Msg
+view : Model -> Html Msg
 view model =
-  div []
-    [ input [ placeholder "Lånebeløp", onInput LoanAmount] []
-    , input [ placeholder "Rente", onInput LoanInterestRate] []
-    , input [ placeholder "Nedbetalingstid", onInput LoanDuration] []
-    , div [] [ text (toString (termAmount model)) ]
-    ]
-
+    div []
+        [ div []
+            [ Textfield.render Mdl
+                [ 0 ]
+                model.mdl
+                [ Textfield.floatingLabel
+                , Textfield.label "Lånebeløp"
+                , Options.onInput LoanAmount
+                ]
+                []
+            ]
+        , div []
+            [ Textfield.render Mdl
+                [ 1 ]
+                model.mdl
+                [ Textfield.floatingLabel
+                , Textfield.label "Rente"
+                , Options.onInput LoanInterestRate
+                ]
+                []
+            ]
+        , div []
+            [ Textfield.render Mdl
+                [ 2 ]
+                model.mdl
+                [ Textfield.floatingLabel
+                , Textfield.label "Nedbetalingstid"
+                , Options.onInput LoanDuration
+                ]
+                []
+            ]
+        , div [] [ text (toString (termAmount model.loanDetails)) ]
+        ]
